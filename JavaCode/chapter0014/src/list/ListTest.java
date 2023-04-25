7046e1113c8d083ee09a62189ca8bcc315aeceec
wggglggg <wggglggg@hotmail.com>
2023-04-25 22:03:03 +0800

    Learn ArrayList original code

---
+++ JavaCode/chapter0014/src/list/ListTest.java
@@ -0,0 +1,23 @@
+package list;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * ClassName: ListTest
+ * Description:
+ *
+ * @Author wggglggg
+ * @Create 2023/4/25 20:45
+ * @Version 1.0
+ */
+public class ListTest {
+    @Test
+    public void test1(){
+        ArrayList list = new ArrayList();
+        list.add("AA");
+    }
+
+
+}
